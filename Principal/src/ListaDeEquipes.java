6e2c5bdc2c662cb1c3a638f64f209b7592580c67
NathanReis <nathanferreirareis_13@hotmail.com>
2023-10-31 20:15:53 -0300

    adicionei o codigo do professor

--- Principal/src/ListaDeEquipes.java
+++ Principal/src/ListaDeEquipes.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ListaDeEquipes {
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class ListaDeEquipes {
+        private Scanner leitor;
+
+        public ListaDeEquipes(File file) {
+            try {
+                leitor = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+            }
+        }
+
+        public ListaDeEquipes(String input) {
+            leitor = new Scanner(input);
+        }
+
+        public boolean hasNext() {
+            return leitor.hasNext();
+        }
+
+        public String nextLine() {
+            return leitor.nextLine();
+        }
+
+
 }
--- Principal/src/Main.java
+++ Principal/src/Main.java
@@ -1,5 +1,20 @@
+import java.io.File;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public class Teste {
+        public static void main(String args[]) {
+
+            File file = new File("entrada.txt");
+            ListaDeEquipes parser = new ListaDeEquipes(file);
+
+            while (parser.hasNext()) {
+                String line = parser.nextLine();
+                String tokens[] = line.split(" ");
+                System.out.println("Linha: "+line);
+                for (int i = 0; i < tokens.length; i++) {
+                    System.out.println("token "+i+" "+tokens[i]);
+                }
+            }
+        }
     }
 }
