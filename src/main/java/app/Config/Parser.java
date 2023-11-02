18b6aa48901a53275df113330cd2fac4e8641364
NathanReis <nathanferreirareis_13@hotmail.com>
2023-11-01 21:04:55 -0300

    removi os métodos que seriam utilizados para HTML da classe Parse

--- src/main/java/app/Config/Parser.java
+++ src/main/java/app/Config/Parser.java
@@ -4,11 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.File;
 
 public class Parser {
@@ -45,27 +40,8 @@ public class Parser {
         return scanner.nextLine();
     }
 
-    public void readAndProcessHTML(File file) {
-        // leitor html = read(file);
 
-        // Document doc = Jsoup.parse(html);
 
-        // Elements elements = doc.getAllElements();
 
-        // for (Element element : elements) {
-        // processElement(element);
-        // }
-    }
-
-    private void processElement(Element element) {
-        // Aqui você pode adicionar o código para processar o elemento, por exemplo:
-        if (element.tagName().equals("var")) {
-            // Se o elemento é uma tag 'var', substitua seu conteúdo por algum valor.
-            element.text("valor substituto");
-        }
-
-        // Imprima o elemento (ou faça o que quiser com ele).
-        System.out.println(element.outerHtml());
-    }
 
 }
